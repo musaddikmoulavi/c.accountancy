f5ef1779b1e4fb8a5c8eee09e8dbc539e800d50f
musaddikmoulavi <76178699+musaddikmoulavi@users.noreply.github.com>
2021-05-08 20:25:20 +0530

    Update contact.php

--- Contact/contact.php
+++ Contact/contact.php
@@ -1,37 +1,27 @@
 <?php
-#$name=$_POST['name'];
-#$visitor_email=$_POST['email'];
-#$subject = $_POST['subject'];
-#$phone = $_POST['phone'];
-#$message = $_POST['message'];
+echo "Hello";
 
-#$email_from = 'musaddikmoulavi@gmail.com';
-#$email_subject = $_POST['subject'];
-#$email_body = "User Name: $name.\n".
-#               "Subject: $subject.\n".
-#              "User Phone: $phone.\n".
- #             "User Email: $visitor_email.\n".
-#              "User Message: $message.\n";
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject = $_POST['subject'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-#$to = "musaddikmoulavi@gmail.com";
-
-#$headers = "From: $email_from \r\n";
-
-#$headers = "Reply-To: $visitor_email \r\n";
-
-#mail($to,$email_subject,$email_body,$headers);
-
-#header("Location:contacts.html");
+$email_from = 'musaddikmoulavi@gmail.com';
+$email_subject = $_POST['subject'];
+$email_body = "User Name: $name.\n".
+               "Subject: $subject.\n".
+              "User Phone: $phone.\n".
+              "User Email: $visitor_email.\n".
+              "User Message: $message.\n";
 
 $to = "musaddikmoulavi@gmail.com";
-$subject = "Response from CA";
-$message = "Thank You";
-$headers = "From: thefate@tech.in";
 
-if(mail($to,$subject,$message,$headers)){
-  echo "Mail send successfully";
-}
-else{
-  echo "Cannot send mail";
-}
+$headers = "From: $email_from \r\n";
+
+$headers = "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location:contacts.html");
 ?>
